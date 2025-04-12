58fa8edba60807707b10065c75c550c10147d251
Shreyash Meshram <91451934+shreyuu@users.noreply.github.com>
2025-04-12 21:46:06 +0530

    Refactor LSP plugin configuration for improved readability and structure

--- lua/shreyuu/init.lua
+++ lua/shreyuu/init.lua
@@ -1,10 +1,5 @@
 require("shreyuu.set")
 require("shreyuu.remap")
-require("shreyuu.lazy_init")
 
 -- Plugin configs
-require("shreyuu.plugin.lsp")
-require("shreyuu.plugin.cmp")
-require("shreyuu.plugin.treesitter")
-require("shreyuu.plugin.autopairs")
-require("shreyuu.plugin.theme")
+require("shreyuu.lazy_init")
--- lua/shreyuu/plugin/lsp.lua
+++ lua/shreyuu/plugin/lsp.lua
@@ -1,25 +1,24 @@
 return {
-    "neovim/nvim-lspconfig",
-    event = { "BufReadPre", "BufNewFile" },
-    dependencies = {
-      "williamboman/mason.nvim",
-      "williamboman/mason-lspconfig.nvim",
-    },
-    config = function()
-      require("mason").setup()
-      require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "pyright", "tsserver", "html", "cssls" },
+  "neovim/nvim-lspconfig",
+  event = { "BufReadPre", "BufNewFile" },
+  dependencies = {
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+  },
+  config = function()
+    require("mason").setup()
+    require("mason-lspconfig").setup({
+      ensure_installed = { "lua_ls", "pyright", "ts_ls", "html", "cssls" },
+    })
+
+    local lspconfig = require("lspconfig")
+    local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+    local servers = { "lua_ls", "pyright", "ts_ls", "html", "cssls" }
+    for _, server in ipairs(servers) do
+      lspconfig[server].setup({
+        capabilities = capabilities,
       })
-  
-      local lspconfig = require("lspconfig")
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
-  
-      local servers = { "lua_ls", "pyright", "tsserver", "html", "cssls" }
-      for _, server in ipairs(servers) do
-        lspconfig[server].setup({
-          capabilities = capabilities,
-        })
-      end
-    end,
-  }
-  
+    end
+  end,
+}
